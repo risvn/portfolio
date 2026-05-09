1f82285230bfb0b3696bff58f3662fc79fcf678d
rsvn <rizwanshaz2004@gmail.com>
2026-05-10 03:34:27 +0530

    blog:setting up post model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    date_posted = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-date_posted"]
+
+    def __str__(self):
+        return self.title
+
+
+
